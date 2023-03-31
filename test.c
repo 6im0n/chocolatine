0c267bec7015f43cd1b67e02a4181f73383a4fb7
6im0n <simon-ubnt@Simon.localdomain>
2023-03-31 21:29:10 +0200

    Add: test file

---
+++ test.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** test
+** File description:
+** test
+*/
+
+
+int main(int argc, char const *argv[])
+{
+    //test error coding style
+    printf("Hello World\n");
+    return 0;
+}
