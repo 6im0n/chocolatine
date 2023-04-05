0be32026758ba0768e4b49fcfc0aab8e70c350d5
Simon <46846093+6im0n@users.noreply.github.com>
2023-04-05 21:25:22 +0200

    update: testing file for better clarity

--- tests/test_bsq.c
+++ tests/test_bsq.c
@@ -8,32 +8,12 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-Test(bsq, test1)
+Test(empty, test1)
 {
     cr_assert_eq(84, 84);
 }
 
-Test(bsq, test2)
-{
-    cr_assert_eq(84, 84);
-}
-
-Test(bsq, test3)
-{
-    cr_assert_eq(84, 84);
-}
-
-Test(bsq, test4)
-{
-    cr_assert_eq(84, 84);
-}
-
-Test(bsq, test5)
-{
-    cr_assert_eq(84, 84);
-}
-
-Test(bsq, test6)
+Test(empty, test2)
 {
     cr_assert_eq(84, 84);
 }
